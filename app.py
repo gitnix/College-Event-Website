c4b199760a43a86bd6cfaa68c05260eef21459ef
gitnix <gitnix1337@gmail.com>
2015-10-29 17:42:46 -0400

    Initial commit with jinja templating

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, json, request,redirect,session
+from flask.ext.mysql import MySQL
+from werkzeug import generate_password_hash, check_password_hash
+
+mysql = MySQL()
+app = Flask(__name__)
+app.secret_key = 'secret'
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
+app.config['MYSQL_DATABASE_DB'] = 'eventwebsite'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
